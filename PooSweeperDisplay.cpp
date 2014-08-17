74a391212a2745ebc22565dae11fb2459ea4ac5d
ChaosBelcebub <mich3ael@web.de>
2014-08-17 11:54:18 +0200

    add PooSweeperDisplay.cpp

---
+++ PooSweeperDisplay.cpp
@@ -0,0 +1,7 @@
+// Copyright 2014, University of Freiburg,
+// Author: Michael Kotzjan
+
+#include "./PooSweeperDisplay.h"
+
+//______________________________________________________________________________
+void PooSweeperDisplay::show(const PooSweeperStateBase* state) {}
