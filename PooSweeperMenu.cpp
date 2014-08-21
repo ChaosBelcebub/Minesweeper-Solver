a01302f473061199d4e8e465506f2577edf7081f
ChaosBelcebub <mich3ael@web.de>
2014-08-21 18:06:42 +0200

    fixed checkstyle error, game doesnt work properly

--- PooSweeperMenu.cpp
+++ PooSweeperMenu.cpp
@@ -1,9 +1,9 @@
 // Copyright 2014, University of Freiburg
 // Author: Michael Kotzjan
 
+#include <ncurses.h>
 #include "./PooSweeperMenu.h"
 #include "./PooSweeper.h"
-#include <ncurses.h>
 
 // _____________________________________________________________________________
 PooSweeperMenu::PooSweeperMenu() {
