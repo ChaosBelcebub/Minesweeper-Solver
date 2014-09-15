c828b33d5e524f5158e06ec3e37de52672fbf8f2
ChaosBelcebub <ChaosBelcebub@users.noreply.github.com>
2014-09-15 14:12:38 +0200

    adding colors complete

--- PooSweeperDisplay.cpp
+++ PooSweeperDisplay.cpp
@@ -22,27 +22,28 @@ void PooSweeperDisplay::show(const PooSweeperStateBase* state) const {
   init_pair(6, COLOR_CYAN, COLOR_BLACK);
   init_pair(7, COLOR_WHITE, COLOR_BLACK);
   init_pair(8, COLOR_YELLOW, COLOR_BLACK);
+  init_pair(9, COLOR_RED, COLOR_BLACK);
   for (int i = 0; i < state->numRows(); ++i) {
     for (int j = 0; j < state->numCols(); ++j) {
       switch (state->getCellInfo(i, j)) {
         case PooSweeperStateBase::UNREVEALED:
+          attron(COLOR_PAIR(0));
+          attron(A_REVERSE);
           mvprintw(i, j, "?");
-          // printf("\x1b[7m");
-          // printf("?")r
-          // printf("\x1b[0m");
+          attroff(A_REVERSE);
+          attroff(COLOR_PAIR(0));
           break;
         case PooSweeperStateBase::MARKED:
+          attron(COLOR_PAIR(9));
+          attron(A_REVERSE);
           mvprintw(i, j, "M");
-          // printf("\x1b[7m");
-          // printf("\x1b[41m");
-          // printf("\u2691");
-          // printf("\x1b[0m");
+          attroff(COLOR_PAIR(9));
+          attroff(A_REVERSE);
           break;
         case PooSweeperStateBase::REVEALED_POO:
+          attron(COLOR_PAIR(3));
           mvprintw(i, j, "X");
-          // printf("\x1b[0;32m");
-          // printf("\u2601");
-          // printf("\x1b[0m");
+          attroff(COLOR_PAIR(3));
           break;
         case PooSweeperStateBase::REVEALED_ZERO:
           attron(COLOR_PAIR(0));
@@ -55,25 +56,39 @@ void PooSweeperDisplay::show(const PooSweeperStateBase* state) const {
           attroff(COLOR_PAIR(1));
           break;
         case PooSweeperStateBase::REVEALED_TWO:
+          attron(COLOR_PAIR(2));
           mvprintw(i, j, "2");
+          attroff(COLOR_PAIR(2));
           break;
         case PooSweeperStateBase::REVEALED_THREE:
+          attron(COLOR_PAIR(3));
           mvprintw(i, j, "3");
+          attroff(COLOR_PAIR(3));
           break;
         case PooSweeperStateBase::REVEALED_FOUR:
+          attron(COLOR_PAIR(4));
           mvprintw(i, j, "4");
+          attroff(COLOR_PAIR(4));
           break;
         case PooSweeperStateBase::REVEALED_FIVE:
+          attron(COLOR_PAIR(5));
           mvprintw(i, j, "5");
+          attroff(COLOR_PAIR(5));
           break;
         case PooSweeperStateBase::REVEALED_SIX:
+          attron(COLOR_PAIR(6));
           mvprintw(i, j, "6");
+          attroff(COLOR_PAIR(6));
           break;
         case PooSweeperStateBase::REVEALED_SEVEN:
+          attron(COLOR_PAIR(7));
           mvprintw(i, j, "7");
+          attroff(COLOR_PAIR(7));
           break;
         case PooSweeperStateBase::REVEALED_EIGHT:
+          attron(COLOR_PAIR(8));
           mvprintw(i, j, "8");
+          attroff(COLOR_PAIR(8));
           break;
       }
     }
